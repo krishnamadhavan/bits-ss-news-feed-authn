e5b67a8e0d523c85403a61d4891a8c481754a687
Krishna Madhavan <krishnamadhavan07@gmail.com>
2023-11-15 09:45:11 +0530

    feat: project folder structure init

---
+++ config/api_router.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter, SimpleRouter
+
+if settings.DEBUG:
+    router = DefaultRouter()
+else:
+    router = SimpleRouter()
+
+
+app_name = "api"
+urlpatterns = router.urls
+
+urlpatterns += [
+    path("users/", include("news_feed_authn.users.urls")),
+]
